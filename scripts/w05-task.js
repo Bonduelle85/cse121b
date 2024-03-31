c237de7b64b23e44c6e5d24030575b4dee0be490
Anton Gorokhov <bonduelle85@gmail.com>
2024-03-31 20:28:25 +0500

    async await + try catch block

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -27,19 +27,24 @@ const displayTemples = async (temples) => {
 
 /* async getTemples Function using fetch()*/
 const getTemples = async () => {
+  try {
     const response = await fetch(url);
-    if (response.ok) {
-      const temples = await response.json();
-      temples.forEach((temple) => {
-        templeList.push(temple);
-      });
-      displayTemples(templeList);
+    if (!response.ok) {
+      throw new Error("Network response not ok");
     }
+    const data = await response.json();
+    data.forEach((temple) => {
+      templeList.push(temple);
+    });
+    displayTemples(templeList);
+  } catch (error) {
+    console.error(`Fetch error: ${error}`);
+  }
             // using then:
   //   fetch(url)
   //     .then((response) => {
   //       if (!response.ok) {
-  //         throw new Error("Network response on ok");
+  //         throw new Error("Network response not ok");
   //       }
   //       return response.json();
   //     })
@@ -53,19 +58,6 @@ const getTemples = async () => {
   //       console.error(`Fetch error: ${error}`);
   //     });
         // async await
-//   try {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error("Network response on ok");
-//     }
-//     const data = await response.json();
-//     data.forEach((temple) => {
-//       templeList.push(temple);
-//     });
-//     displayTemples(templeList);
-//   } catch (error) {
-//     console.error(`Fetch error: ${error}`);
-//   }
 };
 
 /* reset Function */
@@ -97,9 +89,6 @@ const filterTemples = (temples) => {
       break;
 
     case "older":
-      //   let templesOlder1950 = temples.filter(
-      //     (temple) => Number(temple.dedicated.slice(0, 4)) < 1950
-      //   );
       let templesOlder1950 = temples.filter(
         (temple) => new Date(temple.dedicated) < new Date(1950, 0, 1)
       );
